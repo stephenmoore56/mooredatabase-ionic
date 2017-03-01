0d1c62ca2cce1fbd23c0f6635fc1a747ad33fd11
Stephen Moore <stephenmoore56@msn.com>
2017-02-28 20:26:53 -0600

    Tweaking layout

--- src/pages/species-detail/species-detail.ts
+++ src/pages/species-detail/species-detail.ts
@@ -5,59 +5,59 @@ import {DataService} from '../../services/data.service';
 import {ChartService} from '../../services/chart.service';
 
 @Component({
-  selector: 'page-species-detail',
-  templateUrl: './species-detail.html',
-  providers: [
-    DataService,
-    ChartService
-  ]
+    selector: 'page-species-detail',
+    templateUrl: './species-detail.html',
+    providers: [
+        DataService,
+        ChartService
+    ]
 })
 export class SpeciesDetailPage implements OnInit {
 
-  public bird: Result = new Result();
-  public months: Result[] = [this.bird];
-  public speciesId: number;
+    public bird: Result = new Result();
+    public months: Result[] = [this.bird];
+    public speciesId: number;
 
-  constructor(public navCtrl: NavController,
-              public navParams: NavParams,
-              private _dataService: DataService,
-              private _chartService: ChartService) {
-  }
+    constructor(public navCtrl: NavController,
+                public navParams: NavParams,
+                private _dataService: DataService,
+                private _chartService: ChartService) {
+    }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad SpeciesDetailPage');
-  }
+    ionViewDidLoad() {
+        console.log('ionViewDidLoad SpeciesDetailPage');
+    }
 
-  ngOnInit() {
-    this.speciesId = this.navParams.get('id');
-    this._dataService
-      .getSpeciesDetail(this.speciesId)
-      .subscribe(
-        r => this.bird = r[0],
-        error => console.log("Error: ", error),
-        () => {
-          // don't query for sightings or draw chart for birds not seen
-          if (this.bird.last_seen != null) {
-            this._dataService
-              .getMonthsForSpecies(this.speciesId)
-              .subscribe(
-                r => this.months = r,
+    ngOnInit() {
+        this.speciesId = this.navParams.get('id');
+        this._dataService
+            .getSpeciesDetail(this.speciesId)
+            .subscribe(
+                r => this.bird = r[0],
                 error => console.log("Error: ", error),
-                () => this._chartService.drawChartMonthsForSpecies(this.months, 'chart_div_2')
-              );
-          }
-        }
-      );
-  }
+                () => {
+                    // don't query for sightings or draw chart for birds not seen
+                    if (this.bird.last_seen != null) {
+                        this._dataService
+                            .getMonthsForSpecies(this.speciesId)
+                            .subscribe(
+                                r => this.months = r,
+                                error => console.log("Error: ", error),
+                                () => this._chartService.drawChartMonthsForSpecies(this.months, 'chart_div_2')
+                            );
+                    }
+                }
+            );
+    }
 
-  public searchImages(common_name: string) {
-    let url = `http://images.google.com/images?q=${common_name}&sout=1`;
-    window.open(url, '_blank');
-  }
+    public searchImages(common_name: string) {
+        let url = `http://images.google.com/images?q=${common_name}&sout=1`;
+        window.open(url, '_blank');
+    }
 
-  public openWikipediaArticle(common_name: string) {
-    let url = `http://en.wikipedia.org/wiki/${common_name}`;
-    window.open(url, '_blank');
-  }
+    public openWikipediaArticle(common_name: string) {
+        let url = `http://en.wikipedia.org/wiki/${common_name}`;
+        window.open(url, '_blank');
+    }
 
 }
--- src/pages/species-for-month/species-for-month.ts
+++ src/pages/species-for-month/species-for-month.ts
@@ -5,57 +5,57 @@ import {Result} from '../../lib/result';
 import {SpeciesDetailPage} from '../species-detail/species-detail';
 
 @Component({
-  selector: 'page-species-for-month',
-  templateUrl: 'species-for-month.html',
-  providers: [
-    DataService
-  ]
+    selector: 'page-species-for-month',
+    templateUrl: 'species-for-month.html',
+    providers: [
+        DataService
+    ]
 })
 export class SpeciesForMonthPage implements OnInit {
 
-  public birds: Result[] = [];
-  public filteredBirds: Result[] = [];
-  public month: Result;
+    public birds: Result[] = [];
+    public filteredBirds: Result[] = [];
+    public month: Result;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams,
-              private _dataService: DataService) {
-  }
-
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad SpeciesForMonthPage');
-  }
-
-  ngOnInit() {
-    this.month = this.navParams.get("month");
-    this._dataService
-      .getSpeciesForMonth(this.month.monthNumber)
-      .subscribe(
-        r => {
-          this.birds = r;
-          this.filteredBirds = r;
-        },
-        error => console.log("Error: ", error)
-      );
-  }
-
-  public birdSelected(bird: Result) {
-    this.navCtrl.push(SpeciesDetailPage, {
-      id: bird.id
-    });
-  }
-
-  public filterBirds(e: any) {
-    // set val to the value of the searchbar
-    let val = e.target.value;
-
-    // if the value is an empty string don't filter the items
-    if (val && val.trim() != '') {
-      this.filteredBirds = this.birds.filter((bird) => {
-        return (bird.common_name.toLowerCase().indexOf(val.toLowerCase()) > -1);
-      })
-    } else {
-      this.filteredBirds = this.birds;
+    constructor(public navCtrl: NavController, public navParams: NavParams,
+                private _dataService: DataService) {
     }
-  }
+
+    ionViewDidLoad() {
+        console.log('ionViewDidLoad SpeciesForMonthPage');
+    }
+
+    ngOnInit() {
+        this.month = this.navParams.get("month");
+        this._dataService
+            .getSpeciesForMonth(this.month.monthNumber)
+            .subscribe(
+                r => {
+                    this.birds = r;
+                    this.filteredBirds = r;
+                },
+                error => console.log("Error: ", error)
+            );
+    }
+
+    public birdSelected(bird: Result) {
+        this.navCtrl.push(SpeciesDetailPage, {
+            id: bird.id
+        });
+    }
+
+    public filterBirds(e: any) {
+        // set val to the value of the searchbar
+        let val = e.target.value;
+
+        // if the value is an empty string don't filter the items
+        if (val && val.trim() != '') {
+            this.filteredBirds = this.birds.filter((bird) => {
+                return (bird.common_name.toLowerCase().indexOf(val.toLowerCase()) > -1);
+            })
+        } else {
+            this.filteredBirds = this.birds;
+        }
+    }
 
 }
